c4cb78272e07e59b3b3b531488086f9d25ad5a5f
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-05-12 21:44:02 +0200

    Create LSS-Coinaufgaben-Ausblender.user.js

---
+++ LSS-Coinaufgaben-Ausblender.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         LSS-Coinaufgaben-Ausblender
+// @namespace    leitstellenspiel.de
+// @version      1.0
+// @description  Blendet Aufgaben aus, die etwas mit Coins ausgeben zu tun haben.
+// @author       MissSobol
+// @match        https://www.leitstellenspiel.de/tasks/index
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Suche nach Panels mit der Klasse "panel panel-default mission_panel_green task_panel"
+    const panels = document.querySelectorAll('.panel.panel-default.mission_panel_green.task_panel');
+
+    // Iteriere über die gefundenen Panels
+    for (let i = 0; i < panels.length; i++) {
+        const panel = panels[i];
+        const form = panel.querySelector('.task_name');
+
+        // Überprüfe, ob das Formular das Wort "Coin" oder "Coins" enthält
+        if (form && (form.textContent.includes('Coin') || form.textContent.includes('Coins'))) {
+            // Verstecke das Panel
+            panel.style.display = 'none';
+        }
+    }
+})();
